c0f5a85f34598d46451d65a236c3b4930983c76f
boris <bhuarachiperez@gmail.com>
2025-04-07 23:43:23 +0200

    Delete-CORSController+ADD-ClassWebConfig

--- src/main/java/com/example/aeropuerto/controller/VueloController.java
+++ src/main/java/com/example/aeropuerto/controller/VueloController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173") // React
 @RestController
 @RequestMapping("/api/vuelos")
 public class VueloController {
